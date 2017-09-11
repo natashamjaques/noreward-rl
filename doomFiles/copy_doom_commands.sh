46757c68616c47008918691ae759bb7f1a22fd97
Natasha Jaques <jaquesn@mit.edu>
2017-09-11 14:14:16 -0400

    files for running demo

---
+++ doomFiles/copy_doom_commands.sh
@@ -0,0 +1,5 @@
+cp wads/*.wad /home/natasha/anaconda2/lib/python2.7/site-packages/doom_py/scenarios/
+cp __init__.py /home/natasha/anaconda2/lib/python2.7/site-packages/ppaquette_gym_doom/
+cp doom*.py /home/natasha/anaconda2/lib/python2.7/site-packages/ppaquette_gym_doom/
+cp action_space.py /home/natasha/anaconda2/lib/python2.7/site-packages/ppaquette_gym_doom/wrappers/
+
