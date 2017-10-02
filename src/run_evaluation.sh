6feb7c11f7070b36b006fee6986c7e29a4562b44
Natasha Jaques <jaquesn@mit.edu>
2017-10-01 20:49:02 -0400

    script to run several evaluation commands

---
+++ src/run_evaluation.sh
@@ -0,0 +1,3 @@
+python evaluate.py --default --env-id mario --log-dir tmp/random_mario --out-dir /home/natasha/Dropbox/Projects/no_reward_rl/extracted_metrics/evaluation_stats/ --envWrap --descriptor random_mario_lvl_1
+python evaluate.py --default --env-id SuperMarioBros-1-2-v0 --log-dir tmp/random_mario --out-dir /home/natasha/Dropbox/Projects/no_reward_rl/extracted_metrics/evaluation_stats/ --envWrap --descriptor random_mario_lvl_1
+python evaluate.py --default --env-id SuperMarioBros-1-3-v0 --log-dir tmp/random_mario --out-dir /home/natasha/Dropbox/Projects/no_reward_rl/extracted_metrics/evaluation_stats/ --envWrap --descriptor random_mario_lvl_1
