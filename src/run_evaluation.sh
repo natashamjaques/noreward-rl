d1e48b1e31f4028f6c57c77cce0b5f015d549737
Natasha Jaques <jaquesn@mit.edu>
2017-10-03 12:06:54 -0400

    change automatic evaluation script

--- src/run_evaluation.sh
+++ src/run_evaluation.sh
@@ -1,5 +1,5 @@
-python evaluate.py --default --env-id mario --log-dir tmp/random_mario --out-dir /home/natasha/Dropbox/Projects/no_reward_rl/extracted_metrics/evaluation_stats/ --envWrap --descriptor random_mario_lvl_1
-python evaluate.py --default --env-id SuperMarioBros-1-2-v0 --log-dir tmp/random_mario --out-dir /home/natasha/Dropbox/Projects/no_reward_rl/extracted_metrics/evaluation_stats/ --envWrap --descriptor random_mario_lvl_2
-python evaluate.py --default --env-id SuperMarioBros-1-3-v0 --log-dir tmp/random_mario --out-dir /home/natasha/Dropbox/Projects/no_reward_rl/extracted_metrics/evaluation_stats/ --envWrap --descriptor random_mario_lvl_3
 python evaluate.py --default --env-id mario --log-dir tmp/mario_consistency --out-dir /home/natasha/Dropbox/Projects/no_reward_rl/extracted_metrics/evaluation_stats/ --envWrap --descriptor mario_consistency_lvl_1
 python evaluate.py --default --env-id mario --log-dir tmp/mario_curiosity --out-dir /home/natasha/Dropbox/Projects/no_reward_rl/extracted_metrics/evaluation_stats/ --envWrap --descriptor mario_curiosity_lvl_1
+# python evaluate.py --default --env-id mario --log-dir tmp/random_mario --out-dir /home/natasha/Dropbox/Projects/no_reward_rl/extracted_metrics/evaluation_stats/ --envWrap --descriptor random_mario_lvl_1
+# python evaluate.py --default --env-id SuperMarioBros-1-2-v0 --log-dir tmp/random_mario --out-dir /home/natasha/Dropbox/Projects/no_reward_rl/extracted_metrics/evaluation_stats/ --envWrap --descriptor random_mario_lvl_2
+# python evaluate.py --default --env-id SuperMarioBros-1-3-v0 --log-dir tmp/random_mario --out-dir /home/natasha/Dropbox/Projects/no_reward_rl/extracted_metrics/evaluation_stats/ --envWrap --descriptor random_mario_lvl_3
